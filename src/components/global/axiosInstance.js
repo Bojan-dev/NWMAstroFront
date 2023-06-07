b22aa4789c848e6027d797e32fe2d204ce18e63b
Bojan-dev <bojanvreco069@gmail.com>
2023-06-07 18:45:29 +0200

    Changed https to http

--- src/components/global/axiosInstance.js
+++ src/components/global/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL:
-    'https://www.nwmbackend-env.eba-jkm6sgnh.us-east-2.elasticbeanstalk.com',
+    'http://www.nwmbackend-env.eba-jkm6sgnh.us-east-2.elasticbeanstalk.com',
   headers: {
     'Content-Type': 'application/json',
   },
